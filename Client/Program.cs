70f5514e2f50fc037cf9a1f8461c28fa779150f2
Joel Christner <joel.christner@gmail.com>
2020-12-19 16:46:53 -0800

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static bool _UseKeepalives = true;
+        static TcpClient _Client = null;
+        static NetworkStream _NetworkStream = null;
+        static CancellationTokenSource _TokenSource = new CancellationTokenSource();
+        static CancellationToken _Token;
+
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+                _UseKeepalives = args.Any(a => a.Equals("--keepalive"));
+
+            _Client = new TcpClient();
+            _Token = _TokenSource.Token;
+
+            if (_UseKeepalives) SetTcpKeepalives();
+
+            IAsyncResult ar = _Client.BeginConnect("127.0.0.1", 9000, null, null);
+            WaitHandle wh = ar.AsyncWaitHandle;
+             
+            if (!ar.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(5), false))
+            {
+                _Client.Close();
+                throw new TimeoutException();
+            }
+
+            _Client.EndConnect(ar);
+            _NetworkStream = _Client.GetStream();
+                  
+            Task.Run(() => DataReceiver(), _Token);
+
+            Console.WriteLine("Connected to tcp://127.0.0.1:9000, ENTER to exit");
+            Console.ReadLine();
+            _TokenSource.Cancel();
+        }
+
+        static void SetTcpKeepalives()
+        {
+            try
+            {
+#if NETCOREAPP || NET5_0
+
+                _Client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+                _Client.Client.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, 5);
+                _Client.Client.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval, 5);
+                _Client.Client.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveRetryCount, 5);
+
+#elif NETFRAMEWORK
+
+                byte[] keepAlive = new byte[12];
+
+                // Turn keepalive on
+                Buffer.BlockCopy(BitConverter.GetBytes((uint)1), 0, keepAlive, 0, 4);
+
+                // Set TCP keepalive time
+                Buffer.BlockCopy(BitConverter.GetBytes((uint)4), 0, keepAlive, 4, 4); 
+
+                // Set TCP keepalive interval
+                Buffer.BlockCopy(BitConverter.GetBytes((uint)4), 0, keepAlive, 8, 4); 
+
+                // Set keepalive settings on the underlying Socket
+                _Client.Client.IOControl(IOControlCode.KeepAliveValues, keepAlive, null);
+
+#elif NETSTANDARD
+
+#endif
+            }
+            catch (Exception)
+            { 
+            }
+        }
+
+        static async Task DataReceiver()
+        { 
+            while (!_Token.IsCancellationRequested)
+            {
+                byte[] data = new byte[4096]; 
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    int read = await _NetworkStream.ReadAsync(data, 0, data.Length, _Token).ConfigureAwait(false);
+
+                    if (read > 0)
+                    {
+                        await ms.WriteAsync(data, 0, read, _Token).ConfigureAwait(false);
+                        data = ms.ToArray();
+                        break;
+                    }
+                }
+
+                Console.WriteLine("[127.0.0.1:9000] " + Encoding.UTF8.GetString(data));
+            } 
+        } 
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static bool _UseKeepalives = true;
+        static TcpListener _Listener = null;
+        static CancellationTokenSource _TokenSource = new CancellationTokenSource();
+        static CancellationToken _Token;
+
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+                _UseKeepalives = args.Any(a => a.Equals("--keepalive"));
+
+            _Listener = new TcpListener(IPAddress.Loopback, 9000);
+            if (_UseKeepalives) SetTcpKeepalives();
+            _Listener.Start();
+            _Token = _TokenSource.Token;
+
+            Task.Run(() => AcceptConnections(), _Token);
+
+            Console.WriteLine("Waiting for connections on tcp://127.0.0.1:9000, ENTER to exit");
+            Console.ReadLine();
+            _TokenSource.Cancel();
+        }
+
+        static void SetTcpKeepalives()
+        {
+#if NETCOREAPP || NET5_0
+
+            _Listener.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+            _Listener.Server.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, 5);
+            _Listener.Server.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval, 5);
+            _Listener.Server.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveRetryCount, 5);
+
+#elif NETFRAMEWORK
+
+            byte[] keepAlive = new byte[12];
+
+            // Turn keepalive on
+            Buffer.BlockCopy(BitConverter.GetBytes((uint)1), 0, keepAlive, 0, 4);
+
+            // Set TCP keepalive time
+            Buffer.BlockCopy(BitConverter.GetBytes((uint)4), 0, keepAlive, 4, 4); 
+
+            // Set TCP keepalive interval
+            Buffer.BlockCopy(BitConverter.GetBytes((uint)4), 0, keepAlive, 8, 4); 
+
+            // Set keepalive settings on the underlying Socket
+            _Listener.Server.IOControl(IOControlCode.KeepAliveValues, keepAlive, null);
+
+#elif NETSTANDARD
+
+#endif
+        }
+
+        static async void AcceptConnections()
+        {
+            while (!_Token.IsCancellationRequested)
+            {
+                TcpClient tcpClient = await _Listener.AcceptTcpClientAsync().ConfigureAwait(false);
+                string clientIp = tcpClient.Client.RemoteEndPoint.ToString();
+                Console.WriteLine("Connection received from: " + clientIp);
+                await Task.Run(() => DataReceiver(tcpClient, clientIp), _Token);
+            }
+        }
+
+        static async void DataReceiver(TcpClient tcpClient, string clientIp)
+        {
+            while (!_Token.IsCancellationRequested)
+            {
+                byte[] data = new byte[4096];
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    while (true)
+                    {
+                        int read = await tcpClient.GetStream().ReadAsync(data, 0, data.Length, _Token).ConfigureAwait(false);
+
+                        if (read > 0)
+                        {
+                            await ms.WriteAsync(data, 0, read, _Token).ConfigureAwait(false);
+                            data = ms.ToArray();
+                            break;
+                        }
+                    }
+                }
+
+                Console.WriteLine("[" + clientIp + "] " + Encoding.UTF8.GetString(data));
+            }
+        }
+    }
+}
