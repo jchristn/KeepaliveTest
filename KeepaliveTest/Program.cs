a4675ae39690cfc7315fd214e3d20c044209019b
Joel Christner <joel.christner@gmail.com>
2020-12-19 16:02:45 -0800

    Initial commit

---
+++ KeepaliveTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KeepaliveTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
